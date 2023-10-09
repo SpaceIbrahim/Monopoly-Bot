f9fa53d544a7453cfb9b57afdc7332db77f38d13
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-10-09 14:30:24 -0400

    Intialized the bot IT WORKS

---
+++ bot.py
@@ -0,0 +1,40 @@
+import discord
+
+async def send_message(message, user_message):
+    try:
+        await message.channel.send("AAAAAAAA")
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        
+        print(f"{client.user} is now running")
+
+    
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said {user_message} on {channel}")
+        # await message.channel.send("AAAAAAAA")
+
+        if user_message == "balls":
+            await message.channels.send()
+
+    
+    client.run(TOKEN)
+
+    
